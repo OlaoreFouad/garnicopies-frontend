1bd1be4b3c19d93f3bf9e92ebb48c89b9db4afb5
Crusader <abdullah.mudathir@isslng.com>
2024-05-29 15:22:16 +0100

    feat: worked on the nav and header

--- garnicopes/app/components/navigation/nav.tsx
+++ garnicopes/app/components/navigation/nav.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { usePathname } from "next/navigation";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
@@ -20,6 +21,11 @@ const paths = [
     label: "contact",
   },
 ];
+
+interface NavProp {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
 
 /**This section is for when the web application is on desktop View */
 const DesktopNav = () => {
@@ -69,6 +75,7 @@ const MobileNav = () => {
         <Image
           src="/logo.png"
           alt="Vercel Logo"
+          style={{ zIndex: 33 }}
           // className="dark:invert"
           width={150}
           height={34}
@@ -87,57 +94,129 @@ const MobileNav = () => {
         />
       </div>
 
-      {isOpen ? <SideNav /> : ""}
+      <AnimatePresence>
+        {isOpen && <SideNav isOpen={isOpen} setIsOpen={setIsOpen} />}
+      </AnimatePresence>
     </div>
   );
 };
 
-const SideNav = () => {
+const SideNav: React.FC<NavProp> = ({ isOpen, setIsOpen }) => {
   const pathname = usePathname();
 
+  const handleClick = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+
+  const linkVariants = {
+    hidden: (index: number) => ({
+      x: 100,
+      y: 20 * index,
+      opacity: 0,
+    }),
+    visible: (index: number) => ({
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        stiffness: 200,
+        damping: 30,
+        delay: index * 0.1,
+      },
+    }),
+    exit: (index: number) => ({
+      x: 100,
+      y: 20 * index,
+      opacity: 0,
+      transition: {
+        type: "spring",
+        stiffness: 200,
+        damping: 30,
+        delay: index * 0.1,
+      },
+    }),
+  };
+
   return (
-    <div className="sidenav">
+    <motion.div
+      className="sidenav"
+      initial={{ x: "100%" }}
+      animate={{ x: isOpen ? 0 : "-100%" }}
+      exit={{ x: "100%" }}
+      transition={{ type: "spring", stiffness: 200, damping: 30 }}
+    >
       <div className="sidenav__links">
-        {paths.map((path) => (
-          <Link
-            key={path.pathname}
-            href={path.pathname}
-            className={
-              pathname === path.pathname
-                ? "nav__nav_link active"
-                : "nav__nav_link"
-            }
-          >
-            {path.label}
+        <div className="top_section">
+          <Link href={"/"}>
+            <Image
+              src="/logo.png"
+              alt="Vercel Logo"
+              style={{ zIndex: 33 }}
+              // className="dark:invert"
+              width={150}
+              height={34}
+              priority
+            />
           </Link>
+
+          <div className="action" onClick={handleClick}>
+            <Image
+              src="/close.png"
+              alt="close button"
+              width={34}
+              height={34}
+              priority
+            />
+          </div>
+        </div>
+
+        {paths.map((path, index) => (
+          <motion.div
+            key={path.pathname}
+            custom={index}
+            initial="hidden"
+            animate="visible"
+            variants={linkVariants}
+          >
+            <Link
+              href={path.pathname}
+              className={
+                pathname === path.pathname
+                  ? "nav__nav_link active"
+                  : "nav__nav_link"
+              }
+            >
+              {path.label}
+            </Link>
+          </motion.div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
-function Nav() {
-  const [isMobile, setIsMobile] = useState(false);
+const Nav = () => {
+  const [isMobile, setIsMobile] = useState(null);
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
     };
 
-    // Initialize the state based on the current window width
     handleResize();
-
-    // Add event listener for window resize
     window.addEventListener("resize", handleResize);
 
-    // Cleanup event listener on component unmount
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  if (isMobile === null) {
+    return null; // or a loading spinner, if you prefer
+  }
+
   return (
     <div className="nav_body">{isMobile ? <MobileNav /> : <DesktopNav />}</div>
-    // <div></div>
   );
-}
+};
 
 export default Nav;
---
+++ garnicopes/app/landing_page/Header.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+  const handleClick = () => {};
+  return (
+    <>
+      <Image
+        src="/header__bg.png"
+        alt="Vercel Logo"
+        className="bgImg"
+        width={150}
+        height={34}
+        priority
+      />
+      <p className="header__tag">A copywriting agency that just fits.</p>
+      <h1 className="header__heading">
+        Organic <span>Content</span>, Organic <span>Conversions</span>
+      </h1>
+      <div className="button__container">
+        <Link href={"/"} className="button">
+          Let's Talk
+        </Link>
+      </div>
+
+      <Link href={"/"}>
+        <Image
+          src="/scroll.png"
+          alt="Vercel Logo"
+          className="scrollImg"
+          width={90}
+          height={30}
+          priority
+        />
+      </Link>
+
+      {/* <Link href={"/"} className="button">
+        Let's Talk
+      </Link> */}
+    </>
+  );
+}
+
+export default Header;
