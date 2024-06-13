5048948a7da8b258de3f3289e089ab7c8753623f
Crusader <abdullah.mudathir@isslng.com>
2024-06-13 12:25:11 +0100

    feat: working on the respoonsiveness section of the strengths

--- garnicopes/app/about/Strengths.tsx
+++ garnicopes/app/about/Strengths.tsx
@@ -4,28 +4,27 @@ function Strengths() {
   return (
     //STYLING in the landing scss, this is the section that displays the other section
     <section className="strength">
-      Strengths
       <div className="strength__descriptions">
-        <p className="bold">ORGANIC WRITING</p>
+        <p className="bold one">ORGANIC WRITING</p>
       </div>
       <div className="strength__descriptions">
         <p>WEB COPY</p>
-        <p className="bold">content</p>
+        <p className="bold two">content</p>
         <p>messaging</p>
       </div>
       <div className="strength__descriptions">
-        <p className="bold">Marketing</p>
+        <p className="bold three">Marketing</p>
         <p>storytelling</p>
         <p>seo</p>
       </div>
       <div className="strength__descriptions">
         <p>audience</p>
-        <p className="bold">advertising</p>
+        <p className="bold four">advertising</p>
         <p>tone</p>
       </div>
       <div className="strength__descriptions">
         <p>headlines</p>
-        <p className="bold">conversion</p>
+        <p className="bold five">conversion</p>
       </div>
     </section>
   );
