8e0690c8c4900544286d1083fb56c33f40449993
Crusader <abdullah.mudathir@isslng.com>
2024-07-03 19:05:21 +0100

    feat: changed sections to aos

--- garnicopes/app/page.tsx
+++ garnicopes/app/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Image from "next/image";
 import * as Landing from "./landing_page";
 import Contact from "./contact/Contact";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: false,
+    });
+  }, []);
   return (
     <div className=" header">
       <Landing.Header />
